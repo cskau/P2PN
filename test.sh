8c56bc35c0510b1f09a5ba0674e6f7cb204cb2cf
Clement Skau <clementskau@gmail.com>
2012-02-01 14:55:40 +0100

    Adds dynamic node count

--- test.sh
+++ test.sh
@@ -1,26 +1,33 @@
 #!/usr/bin/env bash
 
-# setup seed peer
-./discover.py p8000 8000 >&- & # start in background
+NODES=30
+PORT=8000
 
-for (( c = 8001; c <= 8030; c++ )); do
+# dynamic vars
+PORT_FROM=$(($PORT + 1))
+PORT_TO=$(($PORT + $NODES))
+
+# setup seed peer
+./discover.py p$PORT $PORT >&- & # start in background
+
+for (( c = $PORT_FROM; c <= $PORT_TO; c++ )); do
   ./discover.py p$c $c >&- & # start in background
 done
 
 
-# wait for peers to come up before connection to them
+# wait for seed peer to come up before connecting to them
 sleep 1
 
-for (( c = 8001; c <= 8030; c++ )); do
+for (( c = $PORT_FROM; c <= $PORT_TO; c++ )); do
   sleep 1
-  echo "hello http://localhost:8000" | ./discover.py --interactive $c
+  echo "hello http://localhost:$PORT" | ./discover.py --interactive $c
 done
 
-# wait for peers to come up before connection to them
+# wait for peers to come up before connecting to them
 sleep 1
 
 # start interactive
-./discover.py --interactive 8000
+./discover.py --interactive $PORT
 
 # kill all spawned processes
 kill `jobs -p`
